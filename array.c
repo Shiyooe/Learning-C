35560dac861b7df9f8b4efe612698da8fde03ebc
krom~ <akrommuhammadyusuf@gmail.com>
2026-01-03 23:50:00 +0700

    Add files via upload
    Signed-off-by: krom~ <akrommuhammadyusuf@gmail.com>

---
+++ array.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+
+int main() {
+    double number[6]= {20, 10, 20.2, 43.2, 54.0, 3.14159};
+    char letters= 'A';
+
+    double sum = number[0] + number[1] + number[2] + number[3] + number[4] + number[5];
+
+    printf("the sum of % .2f, %.2f, %.2f, %.2f, %.2f and %.5f is %.5f\n",number[0], number[1], number[2], number[3], number[4], number[5], sum, letters);
+    return 0;
+}
